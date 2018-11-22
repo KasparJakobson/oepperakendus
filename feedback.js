2c94355f28e3a43da46889f397d1ee855eed6b78
Kaspar Jakobson <kaspar.jakobson@khk.ee>
2018-11-22 09:29:45 +0200

    Valmis tagasiside leht

--- feedback.js
+++ feedback.js
@@ -1,5 +1,14 @@
-angular.module('app', []).controller('ctrl', function ($scope) {
-
-    $scope.checkEmail = function (email) {
+angular.module('app', []).controller('ctrl', function ($scope, $http) {
+    $scope.insertData = function (fullname, email, content) {
+        $http.post("sql.php", {
+            'email_fullname': fullname,
+            'email_address': email,
+            'email_body': content
+        }).then(function (response) {
+            console.log(response);
+        }, function (error) {
+            alert("Error sending email");
+            console.error(error);
+        });
     }
 });
---
+++ sql.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 25.09.18
+ * Time: 09:03
+ */
+$data = json_decode(file_get_contents("php://input"));
+
+
+//$mysqli = new mysqli("localhost", "kasparjakobson", "yUfTGU7Qb9fU", "kasparja_oepperakendus");
+$mysqli = new mysqli("localhost", "root", "", "oepperakendus");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+if (!($stmt = $mysqli->prepare("insert into sent_emails (email_fullname, email_address, email_body) values 
+(?, ?, ?) "))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+
+if (!$stmt->bind_param("sss", $data->email_fullname, $data->email_address, $data->email_body)) {
+    return "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+if (!$stmt->execute()) {
+    return "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
